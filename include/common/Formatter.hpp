3ce8ac88d3cca660aebdfb2dd4375930c318e193
Renzo Crisostomo <renzo@crisostomo.me>
2020-05-26 20:29:29 +0200

    Add Common::Formatter namespace

---
+++ include/common/Formatter.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+namespace Common {
+    namespace Formatter {
+        std::string format(const char *fmt, ...);
+    };
+};
---
+++ src/common/Formatter.cpp
@@ -0,0 +1,30 @@
+#include "common/Formatter.hpp"
+#include <cstdarg>
+
+std::string format(const char *fmt, va_list args) {
+    char buffer[4096];
+
+    const auto result = vsnprintf(buffer, sizeof(buffer), fmt, args);
+
+    if (result < 0) {
+        return {};
+    }
+
+    const size_t length = result;
+    if (length < sizeof(buffer)) {
+        return { buffer, length };
+    }
+
+    std::string formatted(length, '\0');
+    vsnprintf(formatted.data(), length + 1, fmt, args);
+
+    return formatted;
+}
+
+std::string Common::Formatter::format(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    std::string formatted = format(fmt, args);
+    va_end(args);
+    return formatted;
+}
