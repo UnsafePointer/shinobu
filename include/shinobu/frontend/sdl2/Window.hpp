f975e629a839edb80b84f4955e09adcb2c836061
Renzo Crisostomo <renzo@crisostomo.me>
2020-06-14 20:31:10 +0200

    Add Shinobu::Frontend::SDL2 namespace with Window class

---
+++ include/shinobu/frontend/sdl2/Window.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include <string>
+#include <cstdint>
+#include <tuple>
+#include "common/Logger.hpp"
+
+namespace Shinobu {
+    namespace Frontend {
+        namespace SDL2 {
+            class Window {
+                Common::Logs::Logger logger;
+                std::string title;
+                uint32_t width;
+                uint32_t height;
+                SDL_GLContext SDLGLContext;
+                SDL_Window *window;
+                uint32_t windowID;
+            public:
+                Window(std::string title, uint32_t width, uint32_t height);
+                ~Window();
+
+                std::pair<uint32_t,uint32_t> dimensions() const;
+                SDL_Window* windowRef() const;
+                SDL_GLContext GLContext() const;
+                void handleSDLEvent(SDL_Event event) const;
+            };
+        };
+    };
+};
---
+++ src/shinobu/frontend/sdl2/Window.cpp
@@ -0,0 +1,39 @@
+#include "shinobu/frontend/sdl2/Window.hpp"
+
+using namespace Shinobu::Frontend::SDL2;
+
+Window::Window(std::string title, uint32_t width, uint32_t height) : logger(Common::Logs::Level::NoLog, ""), title(title), width(width), height(height) {
+    Uint32 flags = SDL_WINDOW_OPENGL;
+    window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, flags);
+    windowID = SDL_GetWindowID(window);
+    SDLGLContext = SDL_GL_CreateContext(window);
+}
+
+Window::~Window() {
+    SDL_GL_DeleteContext(SDLGLContext);
+    SDL_DestroyWindow(window);
+}
+
+std::pair<uint32_t,uint32_t> Window::dimensions() const {
+    return { width, height };
+}
+
+SDL_Window* Window::windowRef() const {
+    return window;
+}
+
+SDL_GLContext Window::GLContext() const {
+    return SDLGLContext;
+}
+
+void Window::handleSDLEvent(SDL_Event event) const {
+    if(event.type != SDL_WINDOWEVENT || event.window.windowID != windowID) {
+        return;
+    }
+    switch (event.window.event) {
+        case SDL_WINDOWEVENT_CLOSE: {
+            logger.logError("Exiting...");
+            break;
+        }
+    }
+}
