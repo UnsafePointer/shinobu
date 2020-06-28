a581857a2a92b803c5dabf72df05fe463fea4517
Renzo Crisostomo <renzo@crisostomo.me>
2020-06-28 09:47:11 +0200

    Add Core::Device::Sound namespace

---
+++ include/core/device/Sound.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstdint>
+
+namespace Core {
+    namespace Device {
+        namespace Sound {
+            class Controller {
+            public:
+                Controller();
+                ~Controller();
+            };
+        };
+    };
+};
---
+++ src/core/device/Sound.cpp
@@ -0,0 +1,6 @@
+#include "core/device/Sound.hpp"
+
+using namespace Core::Device::Sound;
+
+Controller::Controller() {}
+Controller::~Controller() {}
