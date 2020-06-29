68dbe2528a1976f3ffefc4d4e4770029f8619f01
Renzo Crisostomo <renzo@crisostomo.me>
2020-06-29 22:09:55 +0200

    Integrate Gb_Snd_Emu with `shinobu`

--- include/core/device/Sound.hpp
+++ include/core/device/Sound.hpp
@@ -1,13 +1,24 @@
 #pragma once
 #include <cstdint>
+#include <gb_apu/Gb_Apu.h>
+#include <gb_apu/Multi_Buffer.h>
 
 namespace Core {
     namespace Device {
         namespace Sound {
             class Controller {
+                Gb_Apu apu;
+                Stereo_Buffer buffer;
+                blip_time_t time;
+
+                blip_time_t clock();
             public:
                 Controller();
                 ~Controller();
+
+                uint8_t load(uint16_t address);
+                void store(uint16_t address, uint8_t value);
+                void endFrame();
             };
         };
     };
--- src/core/device/Sound.cpp
+++ src/core/device/Sound.cpp
@@ -1,6 +1,29 @@
 #include "core/device/Sound.hpp"
+#include "common/Timing.hpp"
 
 using namespace Core::Device::Sound;
 
-Controller::Controller() {}
+Controller::Controller() : apu(), buffer(), time() {
+    apu.treble_eq(-20.0);
+	buffer.bass_freq(461);
+}
+
 Controller::~Controller() {}
+
+blip_time_t Controller::clock() {
+    return time += 4;
+}
+
+uint8_t Controller::load(uint16_t address) {
+    return apu.read_register(clock(), address);
+}
+
+void Controller::store(uint16_t address, uint8_t value) {
+    apu.write_register(clock(), address, value);
+}
+
+void Controller::endFrame() {
+	time = 0;
+	bool stereo = apu.end_frame(CyclesPerFrame);
+	buffer.end_frame(CyclesPerFrame, stereo);
+}
