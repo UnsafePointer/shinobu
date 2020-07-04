9dff4eec4985ae172a2b4f4f6bd34f1fa67c8650
Renzo Crisostomo <renzo@crisostomo.me>
2020-07-04 08:29:07 +0200

    Add GameController class

---
+++ include/shinobu/frontend/sdl2/GameController.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Shinobu {
+    namespace Frontend {
+        namespace SDL2 {
+            class GameController {
+            public:
+                GameController();
+                ~GameController();
+            };
+        };
+    };
+};
---
+++ src/shinobu/frontend/sdl2/GameController.cpp
@@ -0,0 +1,7 @@
+#include "shinobu/frontend/sdl2/GameController.hpp"
+
+using namespace Shinobu::Frontend::SDL2;
+
+GameController::GameController() { }
+
+GameController::~GameController() { }
