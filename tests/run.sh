95c16d7a6c4cf5f1331d0e387cd174a587bcad2c
Renzo Crisostomo <renzo@crisostomo.me>
2020-07-25 19:12:32 +0200

    Add more tests

--- tests/run.sh
+++ tests/run.sh
@@ -4,4 +4,4 @@ SCRIPT=`realpath $0`
 SCRIPT_PATH=`dirname ${SCRIPT}`
 echo $SCRIPT_PATH
 PROJECT_PATH=`dirname ${SCRIPT_PATH}`
-cat "${SCRIPT_PATH}/test-locations.txt" | xargs -P3 -I {} "${PROJECT_PATH}/build/shinobu" --rom {}
+cat "${SCRIPT_PATH}/test-locations.txt" | xargs -P10 -I {} "${PROJECT_PATH}/build/shinobu" --rom {}
