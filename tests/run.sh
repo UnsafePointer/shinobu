c78db03d784f5bdc8a5c5f455d1994722c9dadfe
Renzo Crisostomo <renzo@crisostomo.me>
2020-07-25 09:18:23 +0200

    Add Bash script to run tests

---
+++ tests/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT=`realpath $0`
+SCRIPT_PATH=`dirname ${SCRIPT}`
+echo $SCRIPT_PATH
+PROJECT_PATH=`dirname ${SCRIPT_PATH}`
+cat "${SCRIPT_PATH}/test-locations.txt" | xargs -P3 -I {} "${PROJECT_PATH}/build/shinobu" --rom {}
