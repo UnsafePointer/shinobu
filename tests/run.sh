abb14ad95b1dfebfd4169770162077541bd47a0b
Renzo Crisostomo <renzo@crisostomo.me>
2020-07-26 15:25:32 +0200

    Add timer tests

--- tests/run.sh
+++ tests/run.sh
@@ -4,5 +4,4 @@ SCRIPT=`realpath $0`
 SCRIPT_PATH=`dirname ${SCRIPT}`
 PROJECT_PATH=`dirname ${SCRIPT_PATH}`
 TESTS_PATH=`realpath $1`
-echo $TESTS_PATH
-cat "${TESTS_PATH}" | xargs -P10 -I {} "${PROJECT_PATH}/build/shinobu" --rom {}
+cat "${TESTS_PATH}" | xargs -P16 -I {} "${PROJECT_PATH}/build/shinobu" --rom {}
