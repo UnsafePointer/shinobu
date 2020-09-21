724f55990dc211d569e6182fc051838d4243a5f1
Renzo Crisostomo <renzo@crisostomo.me>
2020-09-21 17:44:48 +0200

    Add CMake option to enable Sentry in build

--- ci/release/linux.sh
+++ ci/release/linux.sh
@@ -3,7 +3,7 @@ set -x
 set -e
 
 rm -rf build shinobu.debug
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -Bbuild
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -Bbuild -DSENTRY=ON
 cmake --build build --parallel
 objcopy --only-keep-debug build/shinobu shinobu.debug
 sentry-cli upload-dif -o shinobu -p shinobu shinobu.debug
--- src/shinobu/Sentry.cpp
+++ src/shinobu/Sentry.cpp
@@ -1,5 +1,7 @@
 #include "shinobu/Sentry.hpp"
-#include <sentry.h>
+#ifdef SENTRY
+    #include <sentry.h>
+#endif
 #include <stdexcept>
 #include <iostream>
 
@@ -20,24 +22,28 @@ void Manager::initialize(std::string dsn) {
     if (dsn.empty()) {
         return;
     }
-    sentry_options_t *options = sentry_options_new();
-    sentry_options_set_dsn(options, dsn.c_str());
-    sentry_options_set_database_path(options, ".sentry-native");
-    sentry_options_set_auto_session_tracking(options, false);
-    sentry_options_set_symbolize_stacktraces(options, true);
-    sentry_options_set_environment(options, "development");
-    if (sentry_init(options) == 0) {
-        initialized = true;
-    } else {
-        std::string message = "Can't initialize Sentry with non-empty DSN.";
-        std::cout << message << std::endl;
-        throw std::runtime_error(message);
-    }
+    #ifdef SENTRY
+        sentry_options_t *options = sentry_options_new();
+        sentry_options_set_dsn(options, dsn.c_str());
+        sentry_options_set_database_path(options, ".sentry-native");
+        sentry_options_set_auto_session_tracking(options, false);
+        sentry_options_set_symbolize_stacktraces(options, true);
+        sentry_options_set_environment(options, "development");
+        if (sentry_init(options) == 0) {
+            initialized = true;
+        } else {
+            std::string message = "Can't initialize Sentry with non-empty DSN.";
+            std::cout << message << std::endl;
+            throw std::runtime_error(message);
+        }
+    #endif
 }
 
 void Manager::shutdown() {
     if (!initialized) {
         return;
     }
-    sentry_shutdown();
+    #ifdef SENTRY
+        sentry_shutdown();
+    #endif
 }
